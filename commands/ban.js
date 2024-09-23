2519385c41fb2c32920e18a6047eab43d64cc3f6
hihiads <98236975+hihiads@users.noreply.github.com>
2024-09-23 20:51:06 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,25 +1,18 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('ban')
-        .setDescription('Banuje korisnika sa servera.')
-        .addUserOption(option => option.setName('target').setDescription('Korisnik koji treba biti banovan').setRequired(true))
-        .addStringOption(option => option.setName('reason').setDescription('Razlog bana')),
-    async execute(interaction) {
-        const target = interaction.options.getUser('target');
-        const reason = interaction.options.getString('reason') || 'Nema razloga';
+    name: 'ban',
+    description: 'Banovanje člana sa servera.',
+    async execute(message, args) {
+        if (!message.member.permissions.has('BAN_MEMBERS')) return message.reply('Nemaš dozvolu za to!');
 
-        if (!interaction.member.permissions.has('BAN_MEMBERS')) {
-            return interaction.reply('Nemaš dozvolu za ovu komandu.');
-        }
+        const member = message.mentions.members.first();
+        if (!member) return message.reply('Moraš označiti člana kojeg želiš banovati.');
 
-        const member = interaction.guild.members.resolve(target);
-        if (member) {
-            await member.ban({ reason });
-            await interaction.reply(`${target.tag} je banovan zbog: ${reason}`);
-        } else {
-            await interaction.reply('Korisnik nije pronađen ili nije moguće banovati.');
+        try {
+            await member.ban();
+            message.reply(`${member.user.tag} je banovan.`);
+        } catch (err) {
+            message.reply('Ne mogu banovati ovog člana.');
+            console.error(err);
         }
-    },
+    }
 };
