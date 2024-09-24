2f54e5968fd463f39260ad60e97ae4612e8f507b
hihiads <98236975+hihiads@users.noreply.github.com>
2024-09-24 10:25:02 +0200

    Update mute.js

--- slashcommands/mute.js
+++ slashcommands/mute.js
@@ -2,23 +2,27 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('mute')
-        .setDescription('Mute a user in the server.')
+        .setName('mute') // Ime komande
+        .setDescription('Nijemi korisnika na serveru.') // Opis komande
         .addUserOption(option =>
-            option.setName('user')
-                .setDescription('The user to mute')
-                .setRequired(true)),
+            option.setName('user') // Ne prevodimo ime opcije
+                .setDescription('Korisnik kojeg treba nijemiti') // Opis opcije
+                .setRequired(true)), // Obavezna opcija
 
     async execute(interaction) {
-        const user = interaction.options.getUser('user');
-        const member = await interaction.guild.members.fetch(user.id);
+        const user = interaction.options.getUser('user'); // Dohvati korisnika iz opcije
+        const member = await interaction.guild.members.fetch(user.id); // Dohvati člana iz servera
 
+        // Potraži ulogu "Muted"
         let mutedRole = interaction.guild.roles.cache.find(role => role.name === 'Muted');
         if (!mutedRole) {
+            // Ako uloga ne postoji, kreiraj je
             mutedRole = await interaction.guild.roles.create({
                 name: 'Muted',
                 permissions: [],
             });
+
+            // Onemogući slanje poruka i govor u svim kanalima za ulogu "Muted"
             interaction.guild.channels.cache.forEach(async (channel) => {
                 await channel.permissionOverwrites.create(mutedRole, {
                     SEND_MESSAGES: false,
@@ -28,12 +32,12 @@ module.exports = {
         }
 
         try {
-            await member.roles.add(mutedRole);
-            await interaction.reply(`Muted **${user.tag}**.`);
-            await user.send(`You have been muted in **${interaction.guild.name}**.`);
+            await member.roles.add(mutedRole); // Dodaj ulogu "Muted" korisniku
+            await interaction.reply(`Nijemljen **${user.tag}**.`); // Potvrda nijemljenja
+            await user.send(`Nijemljeni ste na **${interaction.guild.name}**.`); // Obavijest korisniku putem DM-a
         } catch (error) {
-            console.error(error);
-            await interaction.reply({ content: 'There was an error trying to mute this user.', ephemeral: true });
+            console.error(error); // Ispis greške u konzolu
+            await interaction.reply({ content: 'Dogodila se greška prilikom pokušaja nijemljenja ovog korisnika.', ephemeral: true }); // Poruka o grešci
         }
     },
 };
