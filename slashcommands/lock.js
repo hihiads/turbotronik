a61d270053415e50aa3e3c951a026d79f0d488bf
hihiads <98236975+hihiads@users.noreply.github.com>
2024-09-24 10:24:38 +0200

    Update lock.js

--- slashcommands/lock.js
+++ slashcommands/lock.js
@@ -2,18 +2,18 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('lock')
-        .setDescription('Lock the current channel.'),
+        .setName('lock') // Ime komande
+        .setDescription('Zaključaj trenutni kanal.'), // Opis komande
 
     async execute(interaction) {
         const channel = interaction.channel;
 
         try {
-            await channel.permissionOverwrites.edit(interaction.guild.roles.everyone, { SEND_MESSAGES: false });
-            await interaction.reply(`Channel **${channel.name}** has been locked.`);
+            await channel.permissionOverwrites.edit(interaction.guild.roles.everyone, { SEND_MESSAGES: false }); // Onemogući slanje poruka za svu publiku
+            await interaction.reply(`Kanal **${channel.name}** je zaključan.`); // Potvrda
         } catch (error) {
             console.error(error);
-            await interaction.reply({ content: 'There was an error trying to lock this channel.', ephemeral: true });
+            await interaction.reply({ content: 'Dogodila se greška prilikom pokušaja zaključavanja ovog kanala.', ephemeral: true }); // Poruka o grešci
         }
     },
 };
