746943d158c7845168778679256804f39af694a0
hihiads <98236975+hihiads@users.noreply.github.com>
2024-09-24 10:26:31 +0200

    Update softban.js

--- slashcommands/softban.js
+++ slashcommands/softban.js
@@ -2,33 +2,33 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('softban')
-        .setDescription('Softban a user from the server.')
+        .setName('softban') // Ime komande
+        .setDescription('Softban korisnika s servera.') // Opis komande
         .addUserOption(option =>
-            option.setName('user')
-                .setDescription('The user to softban')
-                .setRequired(true))
+            option.setName('user') // Ne prevodimo ime opcije
+                .setDescription('Korisnik kojeg treba softbanati') // Opis opcije
+                .setRequired(true)) // Obavezna opcija
         .addStringOption(option =>
-            option.setName('reason')
-                .setDescription('The reason for the softban')),
+            option.setName('reason') // Ne prevodimo ime opcije
+                .setDescription('Razlog za softban')),
 
     async execute(interaction) {
-        const user = interaction.options.getUser('user');
-        const reason = interaction.options.getString('reason') || 'No reason provided';
-        const member = await interaction.guild.members.fetch(user.id);
+        const user = interaction.options.getUser('user'); // Dohvati korisnika iz opcije
+        const reason = interaction.options.getString('reason') || 'Nema navedenog razloga'; // Dohvati razlog
+        const member = await interaction.guild.members.fetch(user.id); // Dohvati člana iz servera
 
         if (!member) {
-            return interaction.reply({ content: 'User not found in this guild.', ephemeral: true });
+            return interaction.reply({ content: 'Korisnik nije pronađen na ovom serveru.', ephemeral: true }); // Poruka ako korisnik nije pronađen
         }
 
         try {
-            await member.ban({ reason });
-            await interaction.guild.members.unban(user.id);
-            await interaction.reply(`Softbanned **${user.tag}** for: ${reason}`);
-            await user.send(`You have been softbanned from **${interaction.guild.name}** for: ${reason}`);
+            await member.ban({ reason }); // Softban korisnika
+            await interaction.guild.members.unban(user.id); // Ponovno odblokiraj korisnika
+            await interaction.reply(`Softbanan **${user.tag}** zbog: ${reason}`); // Potvrda softbana
+            await user.send(`Softbanani ste iz **${interaction.guild.name}** zbog: ${reason}`); // Obavijest korisniku putem DM-a
         } catch (error) {
-            console.error(error);
-            await interaction.reply({ content: 'There was an error trying to softban this user.', ephemeral: true });
+            console.error(error); // Ispis greške u konzolu
+            await interaction.reply({ content: 'Dogodila se greška prilikom pokušaja softbana ovog korisnika.', ephemeral: true }); // Poruka o grešci
         }
     },
 };
