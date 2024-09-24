532964f26ce07b0ccfedabf202b6c61fc7ef5fca
hihiads <98236975+hihiads@users.noreply.github.com>
2024-09-24 10:33:11 +0200

    Update unban.js

--- slashcommands/unban.js
+++ slashcommands/unban.js
@@ -2,24 +2,24 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('unban')
-        .setDescription('Unban a user from the server.')
+        .setName('unban') // Ime komande
+        .setDescription('Ponovno aktiviraj korisnika na serveru.') // Opis komande
         .addStringOption(option =>
-            option.setName('user_id')
-                .setDescription('The ID of the user to unban')
-                .setRequired(true)),
+            option.setName('user_id') // Ne prevodimo ime opcije
+                .setDescription('ID korisnika kojeg treba ponovo aktivirati') // Opis opcije
+                .setRequired(true)), // Obavezna opcija
 
     async execute(interaction) {
-        const userId = interaction.options.getString('user_id');
+        const userId = interaction.options.getString('user_id'); // Dohvati ID korisnika
 
         try {
-            await interaction.guild.members.unban(userId);
-            await interaction.reply(`Unbanned user with ID: ${userId}`);
-            const user = await interaction.client.users.fetch(userId);
-            await user.send(`You have been unbanned from **${interaction.guild.name}**.`);
+            await interaction.guild.members.unban(userId); // Ponovno aktiviraj korisnika
+            await interaction.reply(`Ponovno aktiviran korisnik s ID: ${userId}`); // Potvrda ponovnog aktiviranja
+            const user = await interaction.client.users.fetch(userId); // Dohvati korisnika
+            await user.send(`Ponovno ste aktivirani iz **${interaction.guild.name}**.`); // Obavijest korisniku putem DM-a
         } catch (error) {
-            console.error(error);
-            await interaction.reply({ content: 'There was an error trying to unban this user.', ephemeral: true });
+            console.error(error); // Ispis greške u konzolu
+            await interaction.reply({ content: 'Dogodila se greška prilikom pokušaja ponovnog aktiviranja ovog korisnika.', ephemeral: true }); // Poruka o grešci
         }
     },
 };
