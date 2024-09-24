75c52d4e941db721561e434f799285272df87f85
hihiads <98236975+hihiads@users.noreply.github.com>
2024-09-24 10:26:01 +0200

    Update say.js

--- slashcommands/say.js
+++ slashcommands/say.js
@@ -2,38 +2,14 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('mute')
-        .setDescription('Mute a user in the server.')
-        .addUserOption(option =>
-            option.setName('user')
-                .setDescription('The user to mute')
-                .setRequired(true)),
-
+        .setName('say') // Ime komande
+        .setDescription('Ponovi poruku koju pošalješ.') // Opis komande
+        .addStringOption(option => 
+            option.setName('message') // Ne prevodimo ime opcije
+                .setDescription('Poruka koju treba ponoviti') // Opis opcije
+                .setRequired(true)), // Obavezna opcija
     async execute(interaction) {
-        const user = interaction.options.getUser('user');
-        const member = await interaction.guild.members.fetch(user.id);
-
-        let mutedRole = interaction.guild.roles.cache.find(role => role.name === 'Muted');
-        if (!mutedRole) {
-            mutedRole = await interaction.guild.roles.create({
-                name: 'Muted',
-                permissions: [],
-            });
-            interaction.guild.channels.cache.forEach(async (channel) => {
-                await channel.permissionOverwrites.create(mutedRole, {
-                    SEND_MESSAGES: false,
-                    SPEAK: false,
-                });
-            });
-        }
-
-        try {
-            await member.roles.add(mutedRole);
-            await interaction.reply(`Muted **${user.tag}**.`);
-            await user.send(`You have been muted in **${interaction.guild.name}**.`);
-        } catch (error) {
-            console.error(error);
-            await interaction.reply({ content: 'There was an error trying to mute this user.', ephemeral: true });
-        }
+        const message = interaction.options.getString('message'); // Dohvati poruku iz opcije
+        await interaction.reply(message); // Odgovara s porukom
     },
 };
