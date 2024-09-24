1e03759e855403403fe078400aaf650ff23e0cc8
hihiads <98236975+hihiads@users.noreply.github.com>
2024-09-24 10:15:40 +0200

    Create kick.js

---
+++ slashcommands/kick.js
@@ -0,0 +1,33 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('kick')
+        .setDescription('Kick a user from the server.')
+        .addUserOption(option =>
+            option.setName('user')
+                .setDescription('The user to kick')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('reason')
+                .setDescription('The reason for the kick')),
+
+    async execute(interaction) {
+        const user = interaction.options.getUser('user');
+        const reason = interaction.options.getString('reason') || 'No reason provided';
+        const member = await interaction.guild.members.fetch(user.id);
+
+        if (!member) {
+            return interaction.reply({ content: 'User not found in this guild.', ephemeral: true });
+        }
+
+        try {
+            await member.kick(reason);
+            await interaction.reply(`Kicked **${user.tag}** for: ${reason}`);
+            await user.send(`You have been kicked from **${interaction.guild.name}** for: ${reason}`);
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({ content: 'There was an error trying to kick this user.', ephemeral: true });
+        }
+    },
+};
