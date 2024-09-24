309e8af2053c7e48da11247bece8fc88bedae666
hihiads <98236975+hihiads@users.noreply.github.com>
2024-09-24 10:23:14 +0200

    Update kick.js

--- slashcommands/kick.js
+++ slashcommands/kick.js
@@ -3,31 +3,31 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('kick')
-        .setDescription('Kika korisnika sa servera.')
+        .setDescription('Izbaci korisnika sa servera.')
         .addUserOption(option =>
-            option.setName('user')
-                .setDescription('The user to kick')
+            option.setName('user') // Ne prevodimo ime opcije
+                .setDescription('Korisnik kojeg treba izbaciti')
                 .setRequired(true))
         .addStringOption(option =>
-            option.setName('reason')
-                .setDescription('The reason for the kick')),
+            option.setName('reason') // Ne prevodimo ime opcije
+                .setDescription('Razlog za izbacivanje')),
 
     async execute(interaction) {
-        const user = interaction.options.getUser('user');
-        const reason = interaction.options.getString('reason') || 'No reason provided';
+        const user = interaction.options.getUser('user'); // Ne prevodimo ime opcije
+        const reason = interaction.options.getString('reason') || 'Nema navedenog razloga'; // Poruka ako nije naveden razlog
         const member = await interaction.guild.members.fetch(user.id);
 
         if (!member) {
-            return interaction.reply({ content: 'User not found in this guild.', ephemeral: true });
+            return interaction.reply({ content: 'Korisnik nije pronađen na ovom serveru.', ephemeral: true });
         }
 
         try {
             await member.kick(reason);
-            await interaction.reply(`Kicked **${user.tag}** for: ${reason}`);
-            await user.send(`You have been kicked from **${interaction.guild.name}** for: ${reason}`);
+            await interaction.reply(`Izbačen **${user.tag}** zbog: ${reason}`);
+            await user.send(`Izbačeni ste iz **${interaction.guild.name}** zbog: ${reason}`);
         } catch (error) {
             console.error(error);
-            await interaction.reply({ content: 'There was an error trying to kick this user.', ephemeral: true });
+            await interaction.reply({ content: 'Dogodila se greška prilikom pokušaja izbacivanja ovog korisnika.', ephemeral: true });
         }
     },
 };
