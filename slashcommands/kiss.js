677bd875578f54ee9ddf72c3822a9ad1fd163021
hihiads <98236975+hihiads@users.noreply.github.com>
2024-09-28 21:02:59 +0200

    Update kiss.js

--- slashcommands/kiss.js
+++ slashcommands/kiss.js
@@ -1,20 +1,28 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const fetch = require('node-fetch');
+const TENOR_API_KEY = 'AIzaSyAbuXZwuVzyj8_Q07qbmVgl9GHVvDROUrc'; // Stavi ovde svoj Tenor API ključ
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('kiss')
         .setDescription('Poljubi nekoga.')
-        .addUserOption(option => 
+        .addUserOption(option =>
             option.setName('user')
                 .setDescription('Izaberi korisnika kog želiš da poljubiš')
-                .setRequired(false)), // Ova opcija nije obavezna
+                .setRequired(false)),
 
     async execute(interaction) {
         const user = interaction.options.getUser('user');
+
+        // Fetch GIF sa Tenor API-ja
+        const response = await fetch(`https://g.tenor.com/v1/search?q=anime kiss&key=${TENOR_API_KEY}&limit=1`);
+        const { results } = await response.json();
+        const gifUrl = results[0].media[0].gif.url;
+
         if (user) {
-            await interaction.reply(`${interaction.user.username} je poljubio/la ${user.username} 😘`);
+            await interaction.reply(`${interaction.user.username} je poljubio/la ${user.username} 😘\n${gifUrl}`);
         } else {
-            await interaction.reply(`${interaction.user.username} je poljubio/la nekoga 😘`);
+            await interaction.reply(`${interaction.user.username} je poljubio/la nekoga 😘\n${gifUrl}`);
         }
     },
 };
